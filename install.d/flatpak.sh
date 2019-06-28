306744142ff49e10089f21dbf1fa155eaa732075
Stephen Lewis <code@stephenlewis.me>
2019-06-28 22:29:00 +0000

    Delete separate Flatpak install file

--- install.d/flatpak.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# .........................................................
-# Install Flatpak
-# .........................................................
-sudo apt install flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-echo "👍 Installed Flatpak, so you can now install more things."
