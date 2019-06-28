e3385c58b215d8882f9666c8358a219ca3f4774c
Stephen Lewis <code@stephenlewis.me>
2019-06-28 22:28:26 +0000

    Move Flatpak and Snap to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,22 @@
 sudo apt update
 sudo apt upgrade
 
+# .........................................................
+# Install Flatpak
+# .........................................................
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+echo "👍 Installed Flatpak."
+
+
+# .........................................................
+# Install Snap
+# .........................................................
+sudo apt install snapd
+echo "👍 Installed Snap."
+
+echo "Now we've installed those things, we can install others things..."
+
 # Load everything from install.d/
 setopt extended_glob
 for install_script in ./install.d/*[^~]; do
