5959af7980ada2632dfa597d7d52bb3573f26c4d
Stephen Lewis <code@stephenlewis.me>
2019-06-28 22:29:21 +0000

    Delete separate Snap install file

--- install.d/snap.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# .........................................................
-# Install Snap
-# .........................................................
-sudo apt install snapd
-
-echo "👍 Installed Snap, so you can now install more things."
